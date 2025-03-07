588d8887aae97ebe7a50022005c5b56d76d06c13
pysyun <48061047+pysyun@users.noreply.github.com>
2025-03-08 01:31:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='pysyun_timeline_harmonics',
-    version='1.0',
+    version='1.1',
     author='Illiatea',
     author_email='illiatea2@gmail.com',
-    py_modules=['fourier_intensity_processor'],
+    py_modules=['fourier_intensity_processor', 'circade`],
     install_requires=['scipy', 'numpy']
 )
