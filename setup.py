7dd839dee27b9695a1a546a8b7383cd636dbb6b3
Illia <illiatea2@gmail.com>
2025-04-10 17:56:32 +0300

    Add WeeklyHarmonicsChart and StackedAreaHarmonicsChart; update PeriodToCircadeText to use power_spectrum instead of volume

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='pysyun_timeline_harmonics',
-    version='1.1.1',
+    version='1.1.2',
     author='Illiatea',
     author_email='illiatea2@gmail.com',
-    py_modules=['fourier_intensity_processor', 'circade'],
-    install_requires=['scipy', 'numpy']
+    py_modules=['fourier_intensity_processor', 'circade', 'renders'],
+    install_requires=['scipy', 'numpy', 'matplotlib']
 )
