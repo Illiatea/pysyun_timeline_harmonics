f5a2e8bcf9e0937ec1b431984bfb7f4da179aa41
Illia <illiatea2@gmail.com>
2025-04-10 17:39:47 +0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pysyun_timeline_harmonics',
-    version='1.1',
+    version='1.1.1',
     author='Illiatea',
     author_email='illiatea2@gmail.com',
     py_modules=['fourier_intensity_processor', 'circade'],
